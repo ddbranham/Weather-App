871390b62e9db5e5f68fba1c2bdbeee14ca6f4af
ddbranham <101298037+ddbranham@users.noreply.github.com>
2023-03-31 10:56:33 -0400

    changed icon width

--- script.js
+++ script.js
@@ -83,6 +83,3 @@ dateElement.innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
